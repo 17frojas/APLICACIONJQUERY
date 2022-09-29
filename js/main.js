573802c409bc6728a639872648a888358e3ed47b
Byron Mt <108538978+EquipoSeminario17@users.noreply.github.com>
2022-09-29 15:28:49 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,5 +57,80 @@ $(function(){
    GenerateEnemy();
    
 });
+function MoveBomb(nro){
+    bomb_pos = $("#b-" + nro).position();
+    
+    if (bomb_pos.top > -10){
+             
+        $(".enemy").each(function(){
+            data = $(this).attr("data-uid");
+            
+            enemy_pos = $(this).position();
+            if ( (enemy_pos.top + 100 >= bomb_pos.top) && ( enemy_pos.left <= bomb_pos.left && enemy_pos.left + 100 >= bomb_pos.left) ){
+                //alert("colision"); 
+                    
+                $("#b-" + nro).hide();
+                $(this).fadeOut(1000);
+                
+                if ( $(this).attr("data-colision") == 0 ){
+                    cuac= document.getElementById("cuac");
+                    cuac.currentTime = 0;
+                    cuac.play();
+                    puntaje += 10;
+                    $("#visor").html(puntaje);
+                    $(this).attr("data-colision", 1);
+                }
 
+                  
+            }
+            
+        })
+        
+        $("#b-" + nro).css({"top": bomb_pos.top - 10});
+        setTimeout("MoveBomb(" + nro + ")", 50); //velocidad de la bomba
+    }
+    
+}
+
+function GenerateEnemy(){
+    if (vivo){
+        w = $("body").width();
+        r = aleatorio(10, w - 110);
+        enemy_pos_x = r;
+        enemy_pos_y = 15;
+        
+        e++;
+        $("#ctn-main").append("<div id='e-"+ e +"' data-uid='"+ e + "' data-colision='0' class='enemy' style='top: "+ enemy_pos_y +"; left: "+ enemy_pos_x +"'></div>");
+        MoveEnemy(e);
+    }
+    
+    
+    setTimeout("GenerateEnemy()", 3000);
+}
+
+function MoveEnemy(nro){ //analizar la funcion para congelar enemigos por 3 seg.
+    if (vivo){
+        h = $("body").height();
+        enemy_pos = $("#e-" + nro).position();
+        
+        if (enemy_pos.top < h){
+            $("#e-" + nro).css({"top": enemy_pos.top + 5});
+            setTimeout("MoveEnemy(" + nro + ")", 60);//velocidad del enemigo
+        }else{
+            vivo = false;
+            gameover= document.getElementById("gameover");
+            gameover.currentTime = 0;
+            gameover.play();
+            alert('Fin del Juego ');
+            location.reload();
+
+        }
+    }
+    
+}
+
+function aleatorio(a,b) {
+    nro = Math.round(Math.random()*(b-a)+parseInt(a));
+    return nro;
+}
 
